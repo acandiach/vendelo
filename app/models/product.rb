31144ddfa8c39640234f5f6e9062f595dca86a93
acandia <abelcandiach@gmail.com>
2023-01-11 19:57:24 -0300

    feat: added product order selector

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,12 @@ class Product < ApplicationRecord
     description: 'B'
   }
 
+  ORDER_BY = {
+    newest: "created_at DESC",
+    expensive: "price DESC",
+    cheapest: "price ASC"
+  }
+
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
