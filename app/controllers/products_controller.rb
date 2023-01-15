7ee78f002081fb9853000b3780f8be7957722205
acandia <abelcandiach@gmail.com>
2023-01-15 10:35:40 -0300

    feat: we apply the pattern query object, updated application dependecies and improve security

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,35 +1,18 @@
 class ProductsController < ApplicationController
+ 
   def index
     @categories = Category.all.order(name: :asc).load_async
-    @products = Product.all.with_attached_photo
-    if params[:category_id]
-      @products = @products.where(category_id: params[:category_id])
-    end
-    if params[:min_price].present?
-      @products = @products.where("price >= ?", params[:min_price])
-    end
-    if params[:max_price].present?
-      @products = @products.where("price <= ?", params[:max_price])
-    end
-    if params[:query_text].present?
-      @products = @products.search_full_text(params[:query_text])
-    end
-
-    order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
-
-    @products = @products.order(order_by).load_async
-
-    @pagy, @products = pagy_countless(@products, items: 12)
+    @pagy, @products = pagy_countless(FindProducts.new.call(product_params_index).load_async, items: 12)
   end
-
+ 
   def show
     product
   end
-
+ 
   def new
     @product = Product.new
   end
-
+ 
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -38,11 +21,11 @@ class ProductsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
+ 
   def edit
     product
   end
-
+ 
   def update
     product
     if product.update(product_params)
@@ -52,20 +35,24 @@ class ProductsController < ApplicationController
     end
 
   end
-
+ 
   def destroy
     product.destroy
 
     redirect_to products_path, notice: t('.destroyed'), status: :see_other
   end
-
+ 
   private
-
+ 
   def product
     @product = Product.find(params[:id])
   end
-
+ 
   def product_params
     params.require(:product).permit(:title, :description, :price, :photo, :category_id)
+  end
+ 
+  def product_params_index
+    params.permit(:category_id, :min_price, :max_price, :query_text, :order_by)
   end
 end
