787fe7bb9063ba9fb1a734e75a0c7c1c2bfc04e4
acandia <abelcandiach@gmail.com>
2023-01-25 18:02:03 -0300

    feat: created user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
   end
 
+  resources :users, only: :show, path: '/user', param: :username
   resources :categories, except: %w[show]
   resources :products, path: '/'
 
